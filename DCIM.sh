256c92ab742d4fac9c24a1c5bb2d1ec3b5b61d4f
ahdad <78063619+ahdad@users.noreply.github.com>
2021-01-27 21:13:47 +0700

    DCIM.sh

---
+++ DCIM.sh
@@ -0,0 +1,54 @@
+read -p 'hello silakan tulis (ya) jika ingin masuk:' userinput1;
+#
+if [ "${userinput1:-}" = "ya" ]
+then
+	clear
+fi
+echo -e "\e[31mHELLO SELAMAT DATANG DI TOOL ADTv.2\e[0m";
+echo -e "-------------------------- 
+081517994662
+Pasword: akulupa0
+Ip: 892.628.1.0
+--------------------------
+081575466228
+Paswors: bajingan28
+Ip: 826.836.27
+---------------------------
+081589053412
+Paswors: naisa987
+Ip:185.725.82
+---------------------------";
+echo -e "081577992552
+Paswors: nabil262
+Ip: 823.234.1.0
+---------------------------
+081566256373
+Paswors: arya946
+Ip: 123.732.8.0
+---------------------------
+081527363889
+Pasword: tolmans8
+Ip: 826.745.1.0
+---------------------------
+081547992317
+Pasword: bilikterbang
+Ip: 016.363.8.0
+---------------------------";
+read -p 'ketik (ya) jika ingin keluar:' userinput2;
+#
+if [ "${userinput2:-}" = "ya" ]
+then
+	clear
+fi
+echo -e "\e[91mmaaf tool yang tadi anda tuju sudah rusak jadi wajar ga bisa di pakai maaf ya\e[0m";
+read -p 'ketik ya jika anda me maaf kan saya:' userinput3;
+#
+if [ "${userinput3:-}" = "ya" ]
+then
+	clear
+fi
+if [ "${userinput3:-}" != "ya" ]
+then
+	clear
+;
+fi
